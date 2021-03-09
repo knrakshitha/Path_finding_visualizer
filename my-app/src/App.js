d535e7489220b59e9e34cc6d9fb028a0c5ea8a06
unknown <kn.rakshi@gmail.com>
2021-03-09 11:21:58 +0530

    uploading entire code

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './path-finidng-visualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
